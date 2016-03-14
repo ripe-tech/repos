259a517bc70ee520ca7fa7ec3f8eedd400734e45
joamag <joamag@gmail.com>
2016-03-14 23:54:59 +0000

    multiple kwargs in apps

--- src/repos/main.py
+++ src/repos/main.py
@@ -6,13 +6,14 @@ import appier_extras
 
 class ReposApp(appier.WebApp):
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         appier.WebApp.__init__(
             self,
             name = "repos",
             parts = (
                 appier_extras.AdminPart,
-            )
+            ),
+            *args, **kwargs
         )
 
 if __name__ == "__main__":
